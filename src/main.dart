f4f57295ae7a63779debe6fcfd2dc88890d34045
Ratakondala Arun <arunratakondala3470@gmail.com>
2020-11-01 13:27:35 +0530

    refacted: WebSocketChat

--- src/main.dart
+++ src/main.dart
@@ -3,23 +3,21 @@ import 'dart:io';
 import 'package:shelf/shelf.dart' show Pipeline, logRequests;
 import 'package:shelf/shelf_io.dart' show serve;
 import 'package:shelf_virtual_directory/shelf_virtual_directory.dart';
-import 'room.dart';
 import 'websocket.dart';
 
 const SCRIPTS_PATH = '../web/script/';
 const STYLES_PATH = '../web/style/';
 
-final rooms = <String, Room>{};
-
 void main() {
   try {
     final address = InternetAddress.anyIPv4;
     final port = int.parse(Platform.environment['PORT'] ?? '8080');
+    final chatWebSocket = WebSocketChat.create();
 
     // web and static handler
     final webAndStaticFilesHandler = Pipeline()
         .addMiddleware(logRequests())
-        .addMiddleware(webSocketMiddleWare(rooms))
+        .addMiddleware(chatWebSocket.middleware)
         .addHandler(ShelfVirtualDirectory('../public').handler);
 
     serve(webAndStaticFilesHandler, address, port)
@@ -27,9 +25,7 @@ void main() {
           (server) =>
               print('Serving at wss://${server.address.host}:${server.port}'),
         )
-        .whenComplete(
-          () => rooms.values.forEach((room) async => await room.dispose()),
-        );
+        .whenComplete(() async => await chatWebSocket.dispose());
   } catch (e) {
     print('Failed to create a server');
     print(e);
--- src/websocket.dart
+++ src/websocket.dart
@@ -5,6 +5,83 @@ import 'message.dart';
 import 'client.dart';
 import 'room.dart';
 
+class WebSocketChat {
+  final Map<String, Room> _rooms;
+
+  const WebSocketChat._(this._rooms);
+
+  factory WebSocketChat.create() {
+    return WebSocketChat._({});
+  }
+
+  Middleware get middleware {
+    return createMiddleware(
+      requestHandler: (req) {
+        return req.url.path.startsWith('ws')
+            ? webSocketHandler(
+                (ws) async {
+                  // get user name from the url
+                  final userName = req.url.queryParameters['user'];
+                  final roomName = req.url.queryParameters['room'];
+
+                  final room = _handleRoomCreation(roomName);
+
+                  final client = Client.fromAutoIdWebSocket(userName, ws);
+                  // notify client
+                  client.sendMessage(Message.create(
+                    client.id,
+                    client.name,
+                    'first',
+                    type: MessageType.first,
+                  ));
+
+                  // add client to the rooom
+                  await room.addClient(client);
+
+                  // notify roommates
+                  room.sendBrodCastMessage(
+                    Message.create(
+                      client.id,
+                      client.name,
+                      '${client.name} Joined',
+                      type: MessageType.info,
+                    ),
+                  );
+                },
+              ).call(req)
+            : null;
+      },
+    );
+  }
+
+  Room _handleRoomCreation(String roomName) {
+    if (_rooms.containsKey(roomName)) {
+      return _rooms[roomName];
+    }
+
+    return _rooms[roomName] = Room.create(roomName)
+      ..onClientRemoved = (client) {
+        print('Client removed name:${client.name}  id: ${client.id}');
+      }
+      ..onRoomDisposed = (id) {
+        _rooms.remove(id);
+        print('Room disposed id: $id');
+      };
+  }
+
+  Future<void> dispose() async {
+    try {
+      await Future.forEach<Room>(
+          _rooms.values, (room) async => await room.dispose());
+    } catch (e) {
+      print('Error while disposing ChatWebsocket');
+      print(e);
+    } finally {
+      _rooms.clear();
+    }
+  }
+}
+
 Middleware webSocketMiddleWare(Map<String, Room> rooms) {
   return createMiddleware(
     requestHandler: (req) {
@@ -15,12 +92,8 @@ Middleware webSocketMiddleWare(Map<String, Room> rooms) {
                 final userName = req.url.queryParameters['user'];
                 final roomName = req.url.queryParameters['room'];
 
-                // check if room exists
-                if (!rooms.containsKey(roomName)) {
-                  // assign
-                  rooms[roomName] = Room.empty();
-                }
-                final room = rooms[roomName];
+                final room = _handleRoomCreation(roomName, rooms);
+
                 final client = Client.fromAutoIdWebSocket(userName, ws);
                 // notify client
                 client.sendMessage(Message.create(
@@ -47,4 +120,20 @@ Middleware webSocketMiddleWare(Map<String, Room> rooms) {
           : null;
     },
   );
+}
+
+// creates rome and
+Room _handleRoomCreation(String roomName, Map<String, Room> rooms) {
+  if (rooms.containsKey(roomName)) {
+    return rooms[roomName];
+  }
+
+  return rooms[roomName] = Room.create(roomName)
+    ..onClientRemoved = (client) {
+      print('Client removed name:${client.name}  id: ${client.id}');
+    }
+    ..onRoomDisposed = (id) {
+      rooms.remove(id);
+      print('Room disposed id: $id');
+    };
 }
